19211ea0ce6ae8060a0c3c58bd7c61836804a38c
pyshkovni <guess.the.tutor@gmail.com>
2025-02-24 11:55:19 +0300

    Настроен проект

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h2>Hello, World!</h2>"
+
+@app.route("/")
+def about():
+    return "<h2>About Page.</h2>"
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0")
